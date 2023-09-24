6d967e25e43d988c5033a833f1f34e9c6223e790
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2023-09-25 02:57:16 +0300

    Modify GetAllProductsAction

--- app/Actions/ProductActions/GetAllProductsAction.php
+++ app/Actions/ProductActions/GetAllProductsAction.php
@@ -8,11 +8,7 @@ class GetAllProductsAction
 {
     public function execute(array $filters = null)
     {
-        if ($filters) {
-            $products = Product::with('store' , 'category' , 'images')->filter($filters)->get();
-        } else {
-            $products = Product::all();
-        }
+        $products = ($filters) ? Product::with(['store' , 'category' , 'images'])->filter($filters)->get() : Product::all();
 
         return $products;
     }
