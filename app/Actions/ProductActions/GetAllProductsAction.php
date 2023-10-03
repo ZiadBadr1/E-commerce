7f2baf5f81e5f2355cbf3e70110cfbe831a39bea
shehabwaleed2022 <shehabwaleed2010@gmail.com>
2023-10-03 05:33:15 +0300

    Modify the pagination enum

--- app/Actions/ProductActions/GetAllProductsAction.php
+++ app/Actions/ProductActions/GetAllProductsAction.php
@@ -3,13 +3,13 @@
 namespace App\Actions\ProductActions;
 
 use App\Models\Product;
-use App\Enums\Pagination;
+use App\Enums\PaginationPerPage;
 
 class GetAllProductsAction
 {
     public function execute(array $filters = [])
     {
-        $products = Product::with(['category', 'images'])->filter($filters)->latest()->paginate(Pagination::DEFAULT->value);
+        $products = Product::with(['category', 'images'])->filter($filters)->latest()->paginate(PaginationPerPage::DEFAULT->value);
 
         return $products;
     }
--- app/Enums/PaginationPerPage.php
+++ app/Enums/PaginationPerPage.php
@@ -1,6 +1,6 @@
 <?php
 namespace App\Enums;
 
-enum Pagination: int{
+enum PaginationPerPage: int{
     case DEFAULT = 10;
 }
